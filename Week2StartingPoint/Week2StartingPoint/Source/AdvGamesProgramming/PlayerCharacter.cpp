9b786732c814628c8613546cfea39e0f4982b115
Jonathan Moallem <jdmoallem@gmail.com>
2020-08-04 17:54:23 +1000

    Completed credit level of week 2 project

--- Week2StartingPoint/Week2StartingPoint/Source/AdvGamesProgramming/PlayerCharacter.cpp
+++ Week2StartingPoint/Week2StartingPoint/Source/AdvGamesProgramming/PlayerCharacter.cpp
@@ -2,20 +2,22 @@
 
 
 #include "PlayerCharacter.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+    AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+    bUseControllerRotationPitch = true;
 }
 
 // Called every frame
@@ -30,5 +32,31 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APlayerCharacter::MoveForward);
+    PlayerInputComponent->BindAxis(TEXT("Strafe"), this, &APlayerCharacter::Strafe);
+    PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APlayerCharacter::LookUp);
+    PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APlayerCharacter::Turn);
 }
 
+void APlayerCharacter::MoveForward(float Value)
+{
+    FRotator ForwardRotation = GetControlRotation();
+    ForwardRotation.Roll = 0.f;
+    ForwardRotation.Pitch = 0.f;
+    AddMovementInput(ForwardRotation.Vector(), Value);
+}
+
+void APlayerCharacter::Strafe(float Value)
+{
+    AddMovementInput(GetActorRightVector(), Value);
+}
+
+void APlayerCharacter::LookUp(float Value)
+{
+    AddControllerPitchInput(Value);
+}
+
+void APlayerCharacter::Turn(float Value)
+{
+    AddControllerYawInput(Value);
+}
--- Week2StartingPoint/Week2StartingPoint/Source/AdvGamesProgramming/PlayerCharacter.h
+++ Week2StartingPoint/Week2StartingPoint/Source/AdvGamesProgramming/PlayerCharacter.h
@@ -26,4 +26,11 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void MoveForward(float Value);
+
+    void Strafe(float Value);
+
+    void LookUp(float Value);
+
+    void Turn(float Value);
 };
