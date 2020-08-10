aa1f7c3a5428b55d590d6b7a782b6b10bcdc3acc
Jonathan Moallem <jdmoallem@gmail.com>
2020-08-10 17:08:34 +1000

    Added base files for week 3 lab content

---
+++ Week3StartingPoint/Week3StartingPoint/Source/AdvGamesProgramming/PlayerCharacter.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include "Components/InputComponent.h"
+#include "Engine/World.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	bUseControllerRotationPitch = true;
+
+	LookSensitivity = 1.0f;
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Strafe"), this, &APlayerCharacter::Strafe);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APlayerCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APlayerCharacter::Turn);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &APlayerCharacter::Jump);
+}
+
+void APlayerCharacter::MoveForward(float Value)
+{
+	FRotator ForwardRotation = GetControlRotation();
+	ForwardRotation.Roll = 0.0f;
+	ForwardRotation.Pitch = 0.0f;
+	AddMovementInput(ForwardRotation.Vector(), Value);
+}
+
+void APlayerCharacter::Strafe(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void APlayerCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value * LookSensitivity);
+}
+
+void APlayerCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value * LookSensitivity);
+}
+
