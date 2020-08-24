ce77a21d4ab1bad12afe9466ce146babfd82b849
Jonathan Moallem <jdmoallem@gmail.com>
2020-08-24 13:46:11 +1000

    Added base files for week 5 lab content

---
+++ Week5StartingPoint/Week5StartingPoint/Source/AdvGamesProgramming/EnemyCharacter.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyCharacter.h"
+#include "EngineUtils.h"
+
+
+// Sets default values
+AEnemyCharacter::AEnemyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (Path.Num() == 0 && Manager != NULL)
+	{
+		Path = Manager->GeneratePath(CurrentNode, Manager->AllNodes[FMath::RandRange(0, Manager->AllNodes.Num()-1)]);
+	}
+	else if (Path.Num() > 0 && Manager != NULL)
+	{
+		//UE_LOG(LogTemp, Display, TEXT("Current Node: %s"), *CurrentNode->GetName())
+		if ((GetActorLocation() - CurrentNode->GetActorLocation()).IsNearlyZero(100.0f))
+		{
+			UE_LOG(LogTemp, Display, TEXT("At Node %s"), *CurrentNode->GetName())
+			CurrentNode = Path.Pop();
+		}
+		else
+		{
+			FVector WorldDirection = CurrentNode->GetActorLocation() - GetActorLocation();
+			WorldDirection.Normalize();
+			//UE_LOG(LogTemp, Display, TEXT("The World Direction(X:%f,Y:%f,Z:%f)"), WorldDirection.X, WorldDirection.Y, WorldDirection.Z)
+			AddMovementInput(WorldDirection, 1.0f);
+
+			//Get the AI to face in the direction of travel.
+			FRotator FaceDirection = WorldDirection.ToOrientationRotator();
+			FaceDirection.Roll = 0.f;
+			FaceDirection.Pitch = 0.f;
+			//FaceDirection.Yaw -= 90.0f;
+			SetActorRotation(FaceDirection);
+		}
+	}
+}
+
+// Called to bind functionality to input
+void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Week5StartingPoint/Week5StartingPoint/Source/AdvGamesProgramming/EnemyCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NavigationNode.h"
+#include "GameFramework/Character.h"
+#include "AIManager.h"
+#include "EnemyCharacter.generated.h"
+
+UCLASS()
+class ADVGAMESPROGRAMMING_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	TArray<ANavigationNode*> Path;
+	ANavigationNode* CurrentNode;
+	AAIManager* Manager;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
